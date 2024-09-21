3eb26a53b5f16c4f0f673c5a988938bbdf3d2552
matheusdmlopes <matheusdemarcolopes@gmail.com>
2024-09-21 15:46:43 -0300

    Exercicio de imposto de renda finalizado, tendo 2 alternativas de soluçao, sendo uma proposta pelo professor na sala de aula e a outra sendo feita com else if

--- semestre-1/c/imposto-de-renda.c
+++ semestre-1/c/imposto-de-renda.c
@@ -7,33 +7,77 @@ void solucao1() {
     scanf("%f", &salarioAtual);
 
     if (salarioAtual <= 2259.20) {
-        printf("Voce esta isento do imposto de renda. Seu salario final e o mesmo: %.2f", salarioAtual);
+        printf("Voce esta isento do imposto de renda. \nSeu salario final e o mesmo: R$%.2f", salarioAtual);
     } else if (salarioAtual > 2259.20 && salarioAtual <= 2826.65) {
         imposto = salarioAtual * 0.075;
         salarioFinal = salarioAtual - imposto + 169.44;
-        printf("7.5 porcento de imposto: R$ %.2f\nSalario final: R$ %.2f", imposto, salarioFinal);
     } else if (salarioAtual > 2826.65 && salarioAtual <= 3751.05) {
         imposto = salarioAtual * 0.15;
         salarioFinal = salarioAtual - imposto + 381.44;
-        printf("15 porcento de imposto: R$ %.2f\nSalario final: R$ %.2f", imposto, salarioFinal);
     } else if (salarioAtual > 3751.05 && salarioAtual <= 4664.68) {
         imposto = salarioAtual * 0.225;
         salarioFinal = salarioAtual - imposto + 662.77;
-        printf("22.5 porcento de imposto: R$ %.2f\nSalario final: R$ %.2f", imposto, salarioFinal);
     } else if (salarioAtual > 4664.69) {
         imposto = salarioAtual * 0.275;
         salarioFinal = salarioAtual - imposto + 896;
-        printf("27.5 porcento de imposto: R$ %.2f\nSalario final: R$ %.2f", imposto, salarioFinal);
     } else {
         printf("Erro na aplicacao");
     }
 
-    printf("Imposto: R$%.2f\n", imposto);
-    printf("Salario final: R$%.2f", salarioFinal);
+    if(salarioAtual > 2259.20) {
+        printf("Imposto: R$%.2f\n", imposto);
+        printf("Salario final: R$%.2f", salarioFinal);
+    }
+}
 
-    return 0;
+void solucao2() {
+    float salarioAtual, imposto, salarioFinal;
+
+    printf("Digite seu salario atual: ");
+    scanf("%f", &salarioAtual);
+
+    if(salarioAtual <= 2259.20) {
+        printf("Voce esta isento do imposto de renda. \nSeu salario final e o mesmo: R$%.2f", salarioAtual);
+    } else {
+        if(salarioAtual <= 2826.65) {
+            imposto = salarioAtual * 0.075;
+            salarioFinal = salarioAtual - imposto + 169.44;
+        } else {
+            if(salarioAtual <= 3751.05) {
+                imposto = salarioAtual * 0.15;
+                salarioFinal = salarioAtual - imposto + 381.44;
+            } else {
+                if(salarioAtual <= 4664.68) {
+                    imposto = salarioAtual * 0.225;
+                    salarioFinal = salarioAtual - imposto + 662.77;
+                } else {
+                    imposto = salarioAtual * 0.275;
+                    salarioFinal = salarioAtual - imposto + 896;
+                }
+            }
+        }
+    }
+
+    if(salarioAtual > 2259.20) {
+        printf("Imposto: R$%.2f\n", imposto);
+        printf("Salario final: R$%.2f", salarioFinal);
+    }
+
 }
 
 int main() {
-    solucao1();
+    int escolha;
+
+    printf("Escolha solucao 1 ou 2: ");
+    scanf("%d", &escolha);
+
+    if(escolha == 1) {
+        solucao1();
+    } else if(escolha == 2) {
+        solucao2();
+    } else {
+        printf("Escolha invalida.");
+    }
+
+    return 0;
 }
