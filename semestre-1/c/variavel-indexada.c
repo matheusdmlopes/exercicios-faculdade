2ad558526f1c798d3634cd53afdcb4ca1de0b0fc
matheusdmlopes <matheusdemarcolopes@gmail.com>
2024-10-19 01:11:49 -0300

    Exercicio de variavel indexada, vulgo array

---
+++ semestre-1/c/variavel-indexada.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void arrayNumeros()
+{
+    int x[10];
+    int i;
+
+    for(i = 0; i <= 9; i++)
+    {
+        printf("Valor: ");
+        scanf("%d", &x[i]);
+    }
+
+    printf("Os valores do array sao: \n");
+    for(i = 0; i < 10; i++)
+    {
+        printf("%d\n", x[i]);
+    }
+}
+
+// Esta funçao arrayNomes é somente para curiosidade/teste.
+void arrayNomes()
+{
+    char nomes[10][50];
+    int i;
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("Digite o nome: ");
+        scanf("%49s", &nomes[i]);
+    }
+
+    printf("Os nomes do array sao: \n");
+    for(i=0;i<10;i++) {
+        printf("nome: %s\n", nomes[i]);
+    }
+}
+
+int main()
+{
+    int op;
+
+    while (op < 1 || op > 2)
+    {
+        printf("Escolha uma opcao valida: \n");
+        printf("1 - Exercicio array com numeros somente\n");
+        printf("2 - Exercicio array com nomes\n");
+        printf("Escolha: ");
+        scanf("%d", &op);
+    }
+
+    if(op == 1)
+    {
+        arrayNumeros();
+    }
+    if(op == 2)
+    {
+        arrayNomes();
+    }
+}
