524e197e6ecd4f160d8db701ae4d6560070410d3
Matheus De Marco Lopes <matheusdemarcolopes@gmail.com>
2025-02-26 00:37:36 -0300

    Teste csharp

---
+++ semestre-2/csharp/testeCSharp/Program.cs
@@ -0,0 +1,35 @@
+﻿﻿using System;
+
+class Pessoa
+{
+    public string Nome;
+    public int Idade;
+    public string Sexo;
+    public double Altura;
+
+    public void ExibirInformacoes()
+    {
+        Console.WriteLine($"Informações da pessoa - Nome: {Nome}, Idade: {Idade}, Sexo: {Sexo}, Altura: {Altura}");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.Nome = "Matheus De Marco Lopes";
+        pessoa1.Idade = 21;
+        pessoa1.Sexo = "Masculino";
+        pessoa1.Altura = 1.75;
+
+        Pessoa pessoa2 = new Pessoa();
+        pessoa2.Nome = "Maikon Oliveira";
+        pessoa2.Idade = 30;
+        pessoa2.Sexo = "Masculino";
+        pessoa2.Altura = 1.71;
+
+        pessoa1.ExibirInformacoes();
+        pessoa2.ExibirInformacoes();
+    }
+}
